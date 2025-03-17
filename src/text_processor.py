43475bb9addbe61a141e2ef70c5d388033fe3ba8
wk7x <aleksei.ostlund@gmail.com>
2025-03-17 12:10:06 +0000

    Updated text_processor to work with form_config.

--- src/text_processor.py
+++ src/text_processor.py
@@ -1,30 +1,13 @@
+from form_config import FormConfig
+
 class TextProcessor:
-    #add class attribute that keeps track of the field markers in each form
-    field_markers = {
-        "search_request_form": {
-            "Date of request": "Transplant centre",
-            "Transplant centre": "Searches to be run",
-            "Searches to be run": "Time to transplant",
-            "Time to transplant": "Patient type",
-            "Patient type": "Adults / Paeds",
-            "Adults / Paeds": "PATIENT IDENTIFICATION",
-            "First name": "Middle Name",
-            "Surname": "Sex at Birth",
-            "Sex at Birth": "DOB",
-            "DOB (day/month/year)": "Hospital number",
-            "Hospital number": "Diagnosis",
-            "Diagnosis": "Date of Diagnosis",
-            "Date of Diagnosis": "Weight (kg)",
-            "Weight (kg)": "ABO RhD",
-            "ABO RhD": "CMV Status",
-            "CMV Status": "Patient"
-            }
-        }
     
     def __init__(self, text, form_type, normalize_spacing=True):
         self.text = text
         self.form_type = form_type
-        self.fields = list(self.field_markers[form_type].keys())
+        self.form_configs = FormConfig()
+        self.configs = self.form_configs.get_configs()
+        self.fields = list(self.configs[form_type].keys())
         self.extracted_data = {}
         if normalize_spacing:
             self.normalize_spacing()
@@ -33,7 +16,7 @@ class TextProcessor:
         self.text = ' '.join(self.text.split())
 
     def verify_form_type(self):
-        if self.form_type not in self.field_markers:
+        if self.form_type not in self.configs:
             raise ValueError(f"Invalid form type: {self.form_type}")
         
         fieldcount = len(self.fields)
@@ -47,7 +30,8 @@ class TextProcessor:
     def extract_data(self):
         self.verify_form_type()
         
-        for start_word, end_word in self.field_markers[self.form_type].items():
+        form_markers = self.configs[self.form_type]
+        for field, (start_word, end_word) in form_markers.items():
             start_idx = self.text.index(start_word) + len(start_word)
             end_idx = self.text.index(end_word)
             extracted_data = self.text[start_idx:end_idx].strip()
@@ -62,9 +46,6 @@ class TextProcessor:
             # if no checkbox is filled out set the data to an empty string
             elif "☐" in extracted_data:
                 extracted_data = ""
-            self.extracted_data[start_word] = extracted_data
+            self.extracted_data[field] = extracted_data
         return self.extracted_data
-processor = TextProcessor(input_text, form_type)
-captured_values = processor.extract_data()
-captured_values_string = ", ".join(value for value in captured_values.values())
-print(captured_values_string)
+    
